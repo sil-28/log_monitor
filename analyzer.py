b5b65889eb73a69574311c955235732239b1afa8
sil-28 <silv028@gmail.com>
2026-06-27 15:12:09 +0200

    Added exception cases

--- analyzer.py
+++ analyzer.py
@@ -31,30 +31,37 @@ output_file = 'report_errors.csv'
 print("Starting analysis of the log file\n")
 
 # open log file and create csv file
-with open(input_file, 'r') as log_file, open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
+try:
+    with open(input_file, 'r') as log_file:
+        with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
+            writer_csv = csv.writer(csv_file, delimiter=";")
+            # write table header
+            writer_csv.writerow(['Date', 'Time', 'Level', 'Message'])
+            
+            for row in log_file:
+                if 'ERROR' in row or 'CRITICAL' in row:
+                    
+                    # divide the row based on the blank spaces
+                    parts = row.split()
+                    
+                    date = parts[0]
+                    time = parts[1]
+                    level = parts[2]
+                    message = " ".join(parts[3:])
+                    
+                    # write in the table
+                    writer_csv.writerow([date, time, level, message])
+                    
+                    # if the level is Critical, send an allert on telegram
+                    if level == 'CRITICAL':
+                        mex = f"🚨 ALLERT  🚨\n\n Date: {date} {time}\nLevel: {level}\nAnomaly: {message}"
+                        send_message_telegram(mex)
+                        print("-> notification sent")
+                        
+    print("Analysis completed")
     
-    writer_csv = csv.writer(csv_file, delimiter=";")
-    # write table header
-    writer_csv.writerow(['Date', 'Time', 'Level', 'Message'])
-    
-    for row in log_file:
-        if 'ERROR' in row or 'CRITICAL' in row:
-            
-            # divide the row based on the blank spaces
-            parts = row.split()
-            
-            date = parts[0]
-            time = parts[1]
-            level = parts[2]
-            message = " ".join(parts[3:])
-            
-            # write in the table
-            writer_csv.writerow([date, time, level, message])
-            
-            # if the level is Critical, send an allert on telegram
-            if level == 'CRITICAL':
-                mex = f"🚨 ALLERT  🚨\n\n Date: {date} {time}\nLevel: {level}\nAnomaly: {message}"
-                send_message_telegram(mex)
-                print("-> notification sent")
-    
-print("Analysis completed")
+except FileNotFoundError:
+    print(f"❌ Error: The file '{input_file}' doesn't exist. Verify the path.")
+except PermissionError:
+    print(f"❌ Error: You don't have the permissions to read the file '{input_file}'.")
+
